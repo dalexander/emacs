f07735f5ac60d6e234618b703d9f8513f35123fc
Russell Stewart <russell.s.stewart@gmail.com>
2014-09-27 15:29:46 -0700

    Changed flymakes config.

--- my-python.el
+++ my-python.el
@@ -18,29 +18,7 @@
 (add-to-list 'ac-dictionary-directories (concat my-home-directory ".emacs.d/auto-complete/ac-dict"))
 (ac-config-default)
 
-;; set up pylint
-(setq pycodechecker "pylint_wrapper.py")
-(setq pycodechecker-args " -P /home/russell/app/bin")
-(when (load "flymake" t)
-  (defun flymake-pylint-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-               'flymake-create-temp-inplace))
-       (local-file (file-relative-name
-            temp-file
-            (file-name-directory buffer-file-name))))
-      (list pycodechecker (list pycodechecker-args local-file))))
-
-  (add-to-list 'flymake-allowed-file-name-masks
-           '("\\.py\\'" flymake-pylint-init))
-
-  (defun flymake-show-error-at-point ()
-    (when (get-char-property (point) 'flymake-overlay)
-      (let ((help (get-char-property (point) 'help-echo)))
-    (if help (message "%s" help)))))
-
-  (add-hook 'post-command-hook 'flymake-show-error-at-point)
-  (custom-set-faces
-   '(flymake-errline ((((class color)) (:underline "red"))))
-   '(flymake-warnline ((((class color)) (:underline "orange"))))))
+(require 'flymake-python-pyflakes)
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
 
 (provide 'my-python)
